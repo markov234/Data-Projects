ce07dcf59e79174d8aea3eac579d6d4a569a39ad
Milan Markovic <123490973+markov234@users.noreply.github.com>
2024-11-07 01:43:40 -0600

    couple projects work

--- SQL Server/createlogin.sql
+++ SQL Server/createlogin.sql
@@ -5,3 +5,7 @@
 -- Then run below 2 lines to select table to use.
 -- USE AdventureWorksLT2022;
 -- GRANT SELECT ON SalesLT.Address TO milanmarkovic
+
+-- When we want to for each loop through all tables now, use this code below.
+USE AdventureWorksLT2022;
+GRANT SELECT ON SCHEMA::SalesLT TO milanmarkovic;
--- Untitled-1.py
+++ Untitled-1.py
@@ -2,11 +2,12 @@ import random
 kamala = 0
 trump = 0
 
-for i in range(100000):
+for i in range(80000):
     if random.random() < 0.5:
         kamala += 1
     else:
         trump += 1
 
 print("Trump wins ", trump, " times")
+print("Kamala wins ", kamala, " times")
 
